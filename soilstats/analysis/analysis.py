6ecde346af15adbee9b0318dc3d070038288d2aa
Barbara Vreede <b.vreede@esciencecenter.nl>
2023-12-03 16:40:30 +0100

    add functionality to select max properties

--- soilstats/analysis/analysis.py
+++ soilstats/analysis/analysis.py
@@ -1,5 +1,9 @@
 class Analyse:
     """Mixin class with analysis methods for soil data."""
+    # column names used in the analysis
+    _grouping = ["lat", "lon", "units"]
+    _valuename = "values."
+    _propertyname = "property"
 
     @property
     def df(self):
@@ -9,6 +13,32 @@ class Analyse:
     def get_data(self):
         """Return data from the SoilGrids API as a data frame."""
         return NotImplemented
+
+    def top_property(self, properties = None, value = "mean"):
+        """Return dataframe with the highest values for each location."""
+        if properties is None:
+            properties = self.properties
+
+        valuecol = f"{self._valuename}{value}"
+        df = self._select_content(self.df, self._propertyname, properties)
+        df = self._numeric_and_remove_nans(df, valuecol)
+
+        max_indices = df.groupby(self._grouping)[valuecol].idxmax(skipna=True)
+        columns = self._grouping + [self._propertyname, valuecol]
+
+        return df.loc[max_indices, columns].reset_index(drop=True)
+
+    @classmethod
+    def _numeric_and_remove_nans(cls, df, col):
+        """Convert specific column to numeric and remove NaNs."""
+        df[col] = df[col].astype(float)
+        df.dropna(subset=[col], inplace=True)
+        return df
+
+    @classmethod
+    def _select_content(cls, df, col, content):
+        """Select rows with specific content in a column."""
+        return df[df[col].isin(content)]
 
     def regression(self):
         """Perform regression analysis."""
